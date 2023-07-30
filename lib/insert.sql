05ea0f24dc38d94fcbab0850980fc61fe3a5440f
Ernest <ernestwinata@gmail.com>
2023-07-29 21:36:19 -0700

    create initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true),
+    ('Rowdy', 3, 'M', 'Black', 'Playful', true),
+    ('Tabitha', 7, 'F', 'Gray', 'Calm', true),
+    ('Sergeant Brown', 9, 'M', 'Brown', 'Brave', true),
+    ('Melissa', 4, 'F', 'Blonde', 'Sweet', true),
+    ('Grinch', 6, 'M', 'Green', 'Grumpy', true),
+    ('Wendy', 2, 'F', 'Brown', 'Cheerful', true),
+    (NULL, NULL, NULL, NULL, NULL, true);
+

5deec19e322f5703da51230d7b2cae6c4b2ace41
Ernest <ernestwinata@gmail.com>
2023-07-29 21:52:39 -0700

    fix boolian

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive)
 VALUES
-    ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true),
-    ('Rowdy', 3, 'M', 'Black', 'Playful', true),
-    ('Tabitha', 7, 'F', 'Gray', 'Calm', true),
-    ('Sergeant Brown', 9, 'M', 'Brown', 'Brave', true),
-    ('Melissa', 4, 'F', 'Blonde', 'Sweet', true),
-    ('Grinch', 6, 'M', 'Green', 'Grumpy', true),
-    ('Wendy', 2, 'F', 'Brown', 'Cheerful', true),
-    (NULL, NULL, NULL, NULL, NULL, true);
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Dopey', 1),
+    ('Rowdy', 3, 'M', 'Black', 'Grumpy', 1),
+    ('Tabitha', 7, 'F', 'Gray', 'Sneezy', 1),
+    ('Sergeant Brown', 9, 'M', 'Brown', 'Bashful', 1),
+    ('Melissa', 4, 'F', 'Blonde', 'Sleepy', 1),
+    ('Grinch', 6, 'M', 'Green', 'Happy', 1),
+    ('Wendy', 2, 'F', 'Brown', 'Doc', 1),
+    (NULL, NULL, NULL, NULL, NULL, 1);
 
